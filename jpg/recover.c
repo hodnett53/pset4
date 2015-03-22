3e7010ad2785242aab45cf0131999d22ef020dcf
Luke <hodnett53@gmail.com>
2015-03-22 16:17:02 -0500

    add error checking and framework for recover.c

--- jpg/recover.c
+++ jpg/recover.c
@@ -1,7 +1,8 @@
 /**
  * recover.c
  *
- * Computer Science 50
+ * Luke Hodnett
+ * Harvard CS50x
  * Problem Set 4
  *
  * Recovers JPEGs from a forensic image.
@@ -9,5 +10,27 @@
 
 int main(int argc, char* argv[])
 {
-    // TODO
+    // error check CLA input
+    if (argc != 1)
+    {
+        printf("usage: ./recover.c\n");
+        return 1;
+    }
+    
+    // open RAW file
+    FILE* file = fopen("card.raw", "r");
+    if (file == NULL)
+    {
+        printf("Could not open file\n");
+        return 2;
+    }
+    
+    // TODO search through file for jpg headers
+        // TODO if header found
+            // open a new ###.jpg file
+            // write 512 bytes
+            // scan next four bytes for new header
+                // if not write 512 bytes again (maybe separate this into a separate function
+                // if YES open new ###.jpg file and start again
+            // end loop once all jpgs recovered (16 total)
 }
